833f9834b6d87d4f5b27d4280f3c4459e8f1a04d
Willowfang <willowfang@protonmail.com>
2024-05-13 08:22:37 +0300

    Exercises 2.1-2.5

---
+++ osa2/kurssitiedot/src/components/Course.jsx
@@ -0,0 +1,43 @@
+const Header = ({ name }) => {
+  return <h1>{name}</h1>;
+};
+
+const Part = ({ name, exercises }) => {
+  return (
+    <p>
+      {name} {exercises}
+    </p>
+  );
+};
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map((part) => (
+        <Part key={part.id} name={part.name} exercises={part.exercises} />
+      ))}
+    </div>
+  );
+};
+
+const Total = ({ parts }) => {
+  let total = parts.reduce((prev, next) => (prev += next.exercises), 0);
+
+  return (
+    <p>
+      <b>Total of {total} exercises</b>
+    </p>
+  );
+};
+
+const Course = ({ name, parts }) => {
+  return (
+    <div>
+      <Header name={name} />
+      <Content parts={parts} />
+      <Total parts={parts} />
+    </div>
+  );
+};
+
+export default Course;
